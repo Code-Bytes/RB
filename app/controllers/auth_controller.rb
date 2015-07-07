e0a14e2cb8ed860fde027d575218f87b82035365
Taylor Daugherty <mdaugherty6@gmail.com>
2015-07-07 17:16:44 -0400

    removed binding

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,7 +4,6 @@ class AuthController < ApplicationController
 		#coordinate the params being passed into the github class
 		github = Github.new(params)
 		@user = User.find_or_create_by_auth(github)
-		binding.pry
 		render json: { token: Token.encode(@user.id) }
 	end
 
