0bb3260c5b5d066091ae21ff5a462a74f2780d6d
WebEpic <randy@randyjmitchell.com>
2015-07-09 13:32:39 -0400

    config controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,41 @@ class PostsController < ApplicationController
   before_action :authenticate!, only: [:create, :update, :delete]
 
   def create
+    @post = Post.new(title: params[:title], content: params[:content], user:current_user)
+    if @post.save
+      render json: @post, status: :ok
+    else 
+      render json: @post, status: :unprocessable_entity
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.user == current_user
+      @post.update(post_params)
+      render json: @post, status: :ok
+    else 
+      render json: @post, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.user == current_user
+      @post.destroy(post_params)
+      render json: @post, status: :ok
+    else 
+      render json: @post, status: :unprocessable_entity
+    end
   end
 
   def index
+    @post = Post.order(written_at: :desc)
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :tag_names)
+  end
+
 end
