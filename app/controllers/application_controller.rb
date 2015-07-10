96b2d9ae6192d117f0e255d661078997970c4c8b
Taylor Daugherty <mdaugherty6@gmail.com>
2015-07-09 20:52:01 -0400

    Fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::API
-
+	before_filter :set_current_user
 	#protected or private?
 	protected
 
 	def authenticate!
+		binding.pry
 		unauthorized! unless current_user
 	end
 
@@ -17,10 +18,12 @@ class ApplicationController < ActionController::API
 
 	def set_current_user
 		token = request.headers['Authorization'].to_s.split(' ').last
+		binding.pry
 		return unless token
 
 		payload = Token.new(token)
 
 		@current_user = User.find(payload.user_id) if payload.valid?
+		binding.pry
 	end
 end
