9c364f841815322daee4bff686c71cee073650b9
WebEpic <randy@randyjmitchell.com>
2015-07-13 12:53:43 -0400

    add additional attributes to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :id, :username, :email
-
-  #how do these work??
-  
+  attributes :id, :email, :password, :username, 
+  			 :github, :created_at, :updated_at, 
+  			 :avatar, :company, :url, :location, 
+  			 :follower_count, :following_count, 
+  			 :public_gists, :hireable, :blog
 end
