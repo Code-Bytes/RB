9da3ef1ae84e3c216fa9e86208e042e790d4daf4
WebEpic <randy@randyjmitchell.com>
2015-07-14 11:40:33 -0400

    add parameters to user data returned on the post object

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,5 +2,5 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :user_id, 
   			 :created_at, :updated_at, :cached_votes_total, 
   			 :cached_votes_up, :cached_votes_down, :cached_votes_score
-  has_one :user
+  has_one :user, :only [:username, :avatar, :id]
 end
