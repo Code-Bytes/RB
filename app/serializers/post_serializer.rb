4b7966dbbe59bc0b5ba7507b3261422f39786fc2
Taylor Daugherty <mdaugherty6@gmail.com>
2015-07-14 10:31:30 -0400

    fixed post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,5 +2,7 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :user_id, 
   			 :created_at, :updated_at, :cached_votes_total, 
   			 :cached_votes_up, :cached_votes_down
-  has_one :user
+
+  has_one :user, only: [:id]
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709052010) do
+ActiveRecord::Schema.define(version: 20150713223618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comment_hierarchies", id: false, force: :cascade do |t|
+    t.integer "ancestor_id",   null: false
+    t.integer "descendant_id", null: false
+    t.integer "generations",   null: false
+  end
+
+  add_index "comment_hierarchies", ["ancestor_id", "descendant_id", "generations"], name: "comment_anc_desc_idx", unique: true, using: :btree
+  add_index "comment_hierarchies", ["descendant_id"], name: "comment_desc_idx", using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.string   "posted_by"
+    t.string   "content"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "parent_id"
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
@@ -30,10 +50,16 @@ ActiveRecord::Schema.define(version: 20150709052010) do
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "cached_votes_total", default: 0
+    t.integer  "cached_votes_up",    default: 0
+    t.integer  "cached_votes_down",  default: 0
   end
 
+  add_index "posts", ["cached_votes_down"], name: "index_posts_on_cached_votes_down", using: :btree
+  add_index "posts", ["cached_votes_total"], name: "index_posts_on_cached_votes_total", using: :btree
+  add_index "posts", ["cached_votes_up"], name: "index_posts_on_cached_votes_up", using: :btree
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "tags", force: :cascade do |t|
@@ -61,6 +87,21 @@ ActiveRecord::Schema.define(version: 20150709052010) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
   add_foreign_key "post_tags", "posts"
   add_foreign_key "post_tags", "tags"
