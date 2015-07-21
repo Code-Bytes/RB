90bc8a178bf505f4f111c82c346dff193c28fe4b
WebEpic <randy@randyjmitchell.com>
2015-07-20 21:47:03 -0400

    try to resolve smtp error

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,7 @@
 class UserNotifier < ApplicationMailer
-  default :from => 'no-reply@codebytes.com'
+  default :from => 'codebytesapp@gmail.com'
+
+  # no-reply@codebytes.com
 
   # send a signup email to the user, pass in the user object that containes the user's email address.
   def send_signup_confirmation_email(user)
